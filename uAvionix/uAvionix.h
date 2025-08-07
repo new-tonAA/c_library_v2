d6a42305f0393c3d9f3e2e62d545875f96dd9de3
new-tonAA <2917631059@qq.com>
2025-08-07 16:19:50 +0800

    Update MAVLink headers with custom messages

--- uAvionix/uAvionix.h
+++ uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH 5384874258288807075
+#define MAVLINK_UAVIONIX_XML_HASH 1009415954942552190
 
 #ifdef __cplusplus
 extern "C" {
